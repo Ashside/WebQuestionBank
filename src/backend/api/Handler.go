2f52ef723f35296d7326d2a9a7b8b2b9ff62642c
Ashside <maybeyushu@outlook.com>
2024-05-18 19:49:18 +0800

    ADD: 测试gin框架

--- src/backend/api/Handler.go
+++ src/backend/api/Handler.go
@@ -2,30 +2,18 @@ package api
 
 import (
 	"fmt"
-	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
-func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		http.Error(w, "Failed to parse form", http.StatusBadRequest)
-		return
-	}
+func LoginCheckPost(context *gin.Context) {
 
-	username := r.FormValue("username")
-	password := r.FormValue("password")
+	// 获取POST请求参数
 
-	// 在这里，你可以使用username和password来进行登录验证
-	// 如果验证成功，你可以设置session或者返回一个token
-	// 如果验证失败，你可以返回一个错误信息
+	// 获取POST请求参数中的username
+	email := context.PostForm("email")
+	password := context.PostForm("password")
 
-	_, _ = fmt.Println("Username: ", username)
-	_, _ = fmt.Println("Password: ", password)
-}
-func RegisterHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := fmt.Fprintf(w, "处理器: %s\n", "registerHandler")
-	if err != nil {
-		return
-	}
+	fmt.Println("email: ", email)
+	fmt.Println("password: ", password)
 
 }
--- src/backend/main.go
+++ src/backend/main.go
@@ -1,50 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"github.com/api"
-	"log"
-	"net/http"
-	"time"
-)
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := fmt.Fprintf(w, "处理器: %s\n", "indexHandler")
-	if err != nil {
-		return
-	}
-
-}
-func hiHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := fmt.Fprintf(w, "处理器: %s\n", "hiHandler")
-	if err != nil {
-		return
-	}
-
-}
-func webHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := fmt.Fprintf(w, "处理器: %s\n", "webHandler")
-	if err != nil {
-		return
-	}
-
-}
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Server is running...")
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", indexHandler)
-	mux.HandleFunc("/api/loginCheck", api.LoginHandler)
-	mux.HandleFunc("/api/registerCheck", api.RegisterHandler)
 
-	myServer := &http.Server{
-		Addr:         ":8081",
-		Handler:      mux,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-	}
+	router := gin.Default()
 
-	if err := myServer.ListenAndServe(); err != nil {
-		log.Fatal(err)
-	}
+	// 创建api路由组
+	api := router.Group("/api")
+
+	// 在api路由组下创建usr路由组
+	usr := api.Group("/usr")
+
+	// 在usr路由组下创建loginCheck路由
+	usr.POST("/loginCheck", LoginCheckPost)
 }
