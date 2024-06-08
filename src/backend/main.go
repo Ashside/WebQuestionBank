5fc304713cc5d4faffff88ba09d82be755eee964
MaxSuperMax33 <601191915@qq.com>
2024-06-08 16:56:05 +0800

    main中加入组卷请求

--- src/backend/main.go
+++ src/backend/main.go
@@ -55,5 +55,16 @@ func main() {
 	// 输入：form表单，包含question, answer, option, difficulty, subject, username字段
 	// 输出：json格式，包含success、reason字段
 	addQuestionGroup.POST("/multipleChoice", api.AddChoiceAnswerPost)
+
+	ComposionGroup := questionBankGroup.Group("/composition")
+	//处理/api/questionBank/composition的OPTIONS请求
+	// 该请求用于题目查询（组卷）
+	// 输入：form表单，包含difficulty, subject, keyword, username字段
+	// 输出：json格式，包含success字段
+		// if success = false 包含reason字段
+		// else 包含questions（	ID*int，QuestionType*string ）字段	
+			//if questions空 包含message字段
+	ComposionGroup.POST("/search", api.SearchQuestions)
+
 	_ = r.Run(":8081")
 }
